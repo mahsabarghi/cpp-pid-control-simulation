598df495c3a85432bbb67433b0465e67204fae0f
Mahsa Barghi Mehmandari <mahsabarghimehmandari@gmail.com>
2026-02-19 13:12:45 +0100

    chore: add .gitignore and remove build artifacts

--- build/CMakeFiles/cpp_pid_control_sim.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for cpp_pid_control_sim.
--- build/CMakeFiles/cpp_pid_control_simulation.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for cpp_pid_control_simulation.
-# This may be replaced when dependencies are built.
