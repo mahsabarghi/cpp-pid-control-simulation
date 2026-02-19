71c9dd99931caebe01723fa3448d24a9a41ebc85
Mahsa Barghi Mehmandari <mahsabarghimehmandari@gmail.com>
2026-02-19 13:02:02 +0100

    feat:add PIDController class interface

---
+++ src/PIDController.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <optional>
+
+class PIDController {
+public:
+    struct Limits {
+        double min;
+        double max;
+    };
+
+    PIDController(double kp, double ki, double kd, double dt_seconds);
+
+    void setGains(double kp, double ki, double kd);
+    void setDt(double dt_seconds);
+
+    void setOutputLimits(std::optional<Limits> limits);
+    void setIntegralLimits(std::optional<Limits> limits);
+
+    void reset(double integral = 0.0, double previous_error = 0.0);
+
+    double compute(double setpoint, double measurement);
+
+private:
+    double kp_;
+    double ki_;
+    double kd_;
+    double dt_;
+
+    double integral_;
+    double previous_error_;
+
+    std::optional<Limits> output_limits_;
+    std::optional<Limits> integral_limits_;
+};
+
